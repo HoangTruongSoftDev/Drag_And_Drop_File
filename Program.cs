6a6cdea95839a44b92628eb1b576a0a07c0854e0
HoangTruongSoftDev <hoangtruongpham73@gmail.com>
2024-01-07 01:17:49 -0500

    Initialize application

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DragNDrop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >0)
+            {
+                foreach (var item in args)
+                {
+                    Console.WriteLine("===================================\n");
+                    Console.WriteLine(item);
+                    Console.WriteLine("\n===================================");
+                    string line;
+                    StreamReader file = new StreamReader(item);
+                    Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++\n");
+                    while ((line = file.ReadLine()) != null)
+                     Console.WriteLine(line);
+                    Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++\n");
+                    file.Close();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
